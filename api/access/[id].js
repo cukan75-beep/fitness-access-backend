5e82b590cf4a27fcb57483316e990acdb0bde43f
cukan75-beep <cukan75@gmail.com>
2025-12-06 14:42:32 +0500

    Create [id].js

---
+++ api/access/[id].js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const { id } = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: "No token provided" });
+  }
+
+  res.status(200).json({
+    message: "Access granted",
+    token: id,
+  });
+}
