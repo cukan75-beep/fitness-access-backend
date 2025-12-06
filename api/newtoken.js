d27f9db00898a269d24a7f07916a8dce9bc60f18
cukan75-beep <cukan75@gmail.com>
2025-12-06 17:15:58 +0500

    Update newtoken.js

--- api/newtoken.js
+++ api/newtoken.js
@@ -1,4 +1,14 @@
 export default function handler(req, res) {
-  const id = Math.random().toString(36).substring(2, 10);
-  res.status(200).json({ link: `https://fitness-access-backend.vercel.app/api/access/${id}` });
+  // timestamp в секундах
+  const timestamp = Math.floor(Date.now() / 1000);
+
+  // случайная строка
+  const random = Math.random().toString(36).substring(2, 8);
+
+  // токен = 10 цифр времени + рандом
+  const token = `${timestamp}${random}`;
+
+  res.status(200).json({
+    link: `https://fitness-access-backend.vercel.app/api/access/${token}`
+  });
 }
